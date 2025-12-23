3774195076c74591bae4f97aec3c0cecd8b8a745
amina boukrouh <amienaboukrouh@gmail.com>
2025-12-23 16:03:04 +0100

    creat project

---
+++ src/main/java/rs/spai/demoQL/DemoQlApplication.java
@@ -0,0 +1,13 @@
+package rs.spai.demoQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoQlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/spai/demoQL/DemoQlApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.spai.demoQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
